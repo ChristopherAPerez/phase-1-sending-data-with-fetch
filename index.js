88eb39fa06603054d476efff83f477782f234a69
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-02-04 22:54:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email){
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json",
+            accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        console.log(object);
+    })
+    .catch(function(error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+    })
+}
